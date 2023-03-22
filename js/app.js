3409362b38ba41dcec82357d81ffd71874d749af
Joevdhooff <joe.van.den.hooff@sintlucasedu.nl>
2023-03-22 16:08:20 +0100

    added tilemap and movement

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 document.getElementById("mainTitle").innerText = "Point And Click Adventure"
 
 const gameWindow = document.getElementById("gameWindow");
+const mainCharacter = document.getElementById("mainCharacter");
+
+gameWindow.onclick = function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left; // x postition within the element
+    var y = e.clientY - rect.top; // y postition within the element
+    console.log(x + " " + y)
+    mainCharacter.style.left = x + "px";
+    mainCharacter.style.top = y + "px";
+}
