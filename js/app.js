a046fad610e6be00c6620f43dc71f4900a95eacd
Joevdhooff <joe.van.den.hooff@sintlucasedu.nl>
2023-04-06 00:07:20 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
 document.getElementById("mainTitle").innerText = "Point And Click Adventure"
 
+const tilemap = document.getElementById("map");
 const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
 const offsetCharacter = 7;
 const doorMessage = document.getElementById("doorAlert");
+const tree = document.getElementById("keyTree");
 
 const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
-const counterSpeech = document.getElementById("counterSpeech");
-const targetBalloon = document.getElementById("speech-bubble");
+
 const sec = 1000;
 
-var audio = new Audio("http://music.ogg");
+const counterSpeech = document.getElementById("counterSpeech");
+const counterCharacter = document.getElementById("counterCharacter");
+
+const characterAudio = document.getElementById("characterAudio");
+const counterAudio = document.getElementById("counterAudio");
+
+let key = false;
+let gotKey = false;
 
 
 gameWindow.onclick = function(e){
-    if (mainCharacterSpeech.style.opacity == 0){
+    if (mainCharacterSpeech.style.opacity == 0 && counterCharacter.style.opacity == 0){
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left; // x postition within the element
     var y = e.clientY - rect.top; // y postition within the element
@@ -25,26 +33,52 @@ gameWindow.onclick = function(e){
 
         switch(e.target.id){
             case "door":
-                showMessage(mainCharacterSpeech, "You need a key for this door...");
-                //setTimeout(showMessage, 4 * sec, mainCharacterSpeech, "Wait what?! There a ruby here.");
-
+                if(gotKey === true){
+                    showMessage(mainCharacterSpeech, characterAudio, "I have unlocked this door!...");
+                    tree.style.opacity = 0;
+                    tilemap.src = "img/InsideHouseTileMap.png";
+                }
+                else{
+                    key = true;
+                    showMessage(mainCharacterSpeech, characterAudio, "Oh this door is locked...");
+                    setTimeout(function () { counterCharacter.style.opacity = 1; }, 4 * sec);
+                    setTimeout(showMessage, 4 * sec, counterSpeech, counterAudio, "You need a key for this door...");
+                    setTimeout(showMessage, 8 * sec, mainCharacterSpeech, characterAudio, "Where can i find this key?...");
+                    setTimeout(showMessage, 12 * sec, counterSpeech, counterAudio, "The key is hidden behind one of the trees...");
+                    setTimeout(function () { counterCharacter.style.opacity = 0; }, 16 * sec);
+                }
+                
                 break;
-            case "tree1":
-                console.log("boom")
+            case "keyTree":
+                //IMPORTANT!!!!
+                // I dont know why but only when i click somewhere near the edge of the tree it wil do something.
+                console.log("this is a tree");
+                if(key === true){
+                    showMessage(mainCharacterSpeech, characterAudio, "I have found the housekeys!...");
+                    gotKey = true;
+                }
+                else{
+                    return;
+                }
                 break;
             default:
-                doorMessage.style.display = "none";
+                hideMessage(mainCharacterSpeech, characterAudio);
+                hideMessage(counterSpeech, counterAudio);
+                break;
         }
         }
 }
 
-function showMessage(targetBalloon, message) {
-    mainCharacterSpeech.innerHTML = message;
-    mainCharacterSpeech.style.opacity = 1;
-    setTimeout(hideMessage, 4 * sec, targetBalloon);
+function showMessage(targetBubble, targetAudio, message) {
+    targetAudio.currentTime = 0;
+    targetAudio.play();
+    targetBubble.innerHTML = message;
+    targetBubble.style.opacity = 1;
+    setTimeout(hideMessage, 4 * sec, targetBubble, targetAudio);
 }
-function hideMessage() {
-    mainCharacterSpeech.style.opacity = 0;
-    mainCharacterSpeech.innerHTML = "...";
+function hideMessage(targetBubble, targetAudio) {
+    targetAudio.pause();
+    targetBubble.style.opacity = 0;
+    targetBubble.innerHTML = "...";
 }
 
