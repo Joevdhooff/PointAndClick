7d46294154039203a7ca9860498c59e6c73c4518
Joevdhooff <joe.van.den.hooff@sintlucasedu.nl>
2023-04-05 12:47:50 +0200

    .

--- js/app.js
+++ js/app.js
@@ -4,27 +4,48 @@ const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
 const offsetCharacter = 7;
 const doorMessage = document.getElementById("doorAlert");
+
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
+const counterSpeech = document.getElementById("counterSpeech");
+const targetBalloon = document.getElementById("speech-bubble");
+const sec = 1000;
+
 var audio = new Audio("http://music.ogg");
 
 
 gameWindow.onclick = function(e){
+    if (mainCharacterSpeech.style.opacity == 0){
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left; // x postition within the element
     var y = e.clientY - rect.top; // y postition within the element
     console.log(x + " " + y)
-
     mainCharacter.style.left = x - offsetCharacter + "px";
     mainCharacter.style.top = y - offsetCharacter + "px";
-    switch(e.target.id){
-        case "door":
-            doorMessage.style.display = "flex";
-            break;
-        case "tree1":
-            console.log("boom")
-            break;
-        default:
-            doorMessage.style.display = "none";
-    }
+
+
+        switch(e.target.id){
+            case "door":
+                showMessage(mainCharacterSpeech, "You need a key for this door...");
+                //setTimeout(showMessage, 4 * sec, mainCharacterSpeech, "Wait what?! There a ruby here.");
+
+                break;
+            case "tree1":
+                console.log("boom")
+                break;
+            default:
+                doorMessage.style.display = "none";
+        }
+        }
 }
 
+function showMessage(targetBalloon, message) {
+    targetBalloon.style.opacity = "1";
+    targetBalloon.innerHTML = message;
+    setTimeout(hideMessage, 4 * sec, targetBalloon);
+}
+
+function hideMessage(targetBalloon) {
+    targetBalloon.style.opacity = "0";
+    targetBalloon.innerHTML = "...";
+}
 
